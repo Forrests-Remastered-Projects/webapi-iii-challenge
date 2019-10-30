179cc2f4ca6d280c6d94e471107c45126b9853c0
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-30 15:34:42 -0700

    Fixed bugs!

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,4 +1,4 @@
-const express = "express";
+const express = require("express");
 const User = require("./userDb");
 const router = express.Router();
 const Post = require("../posts/postDb");
@@ -15,59 +15,65 @@ router.post("/", validateUser, (req, res) => {
     });
 });
 
-router.post("/:id/posts", validateUserId, validatePost, (req, res) => {});
-const post = req.body;
-Post.insert(post)
-  .then(post => {
-    res.status(201).json(post);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ error: "Error adding post" });
-  });
-router.get("/", (req, res) => {});
-User.get()
-  .then(users => {
-    res.status(200).json(users);
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ error: "Error getting users" });
-  });
-router.get("/:id", validateUserId, (req, res) => {});
-res.status(200).json(req.user);
-router.get("/:id/posts", (req, res) => {});
-const { id } = req.params;
-User.getUserPosts(id)
-  .then(posts => res.status(200).json(posts))
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ error: "Error getting user posts" });
-  });
-router.delete("/:id", validateUserId, (req, res) => {});
-const { id } = req.user;
-User.remove(id)
-  .then(() => res.status(204).end())
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ error: "Error deleting user" });
-  });
-router.put("/:id", validateUserId, (req, res) => {});
-const { id } = req.params;
-const { name } = req.body;
-User.update(id, { name })
-  .then(() => {
-    User.getById(id)
-      .then(user => res.status(200).json(user))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ error: "Error getting user" });
-      });
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({ error: "Error updating user" });
-  });
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
+  const post = req.body;
+  Post.insert(post)
+    .then(post => {
+      res.status(201).json(post);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error adding post" });
+    });
+});
+router.get("/", (req, res) => {
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error getting users" });
+    });
+});
+router.get("/:id", validateUserId, (req, res) => {
+  res.status(200).json(req.user);
+});
+router.get("/:id/posts", (req, res) => {
+  const { id } = req.params;
+  User.getUserPosts(id)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error getting user posts" });
+    });
+});
+router.delete("/:id", validateUserId, (req, res) => {
+  const { id } = req.user;
+  User.remove(id)
+    .then(() => res.status(204).end())
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error deleting user" });
+    });
+});
+router.put("/:id", validateUserId, (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  User.update(id, { name })
+    .then(() => {
+      User.getById(id)
+        .then(user => res.status(200).json(user))
+        .catch(err => {
+          console.log(err);
+          res.status(500).json({ error: "Error getting user" });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Error updating user" });
+    });
+});
 //custom middleware
 
 function validateUserId(req, res, next) {
